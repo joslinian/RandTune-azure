4e3b63bc25fdd8d110d4736829a003c11bd183a9
Ian Joslin <joslinian7@gmail.com>
2018-07-11 14:51:51 -0700

    Update googleOauth2.js

--- googleOauth2.js
+++ googleOauth2.js
@@ -3,8 +3,8 @@ exports.__esModule = true;
 var googleOauth2 = /** @class */ (function () {
     function googleOauth2() {
     }
-    googleOauth2.id = '1048829641570-0o3gqdk5i3a8gfcfbk300lp4rsmdijgp.apps.googleusercontent.com';
-    googleOauth2.secret = 'aLiHEpvvXwO9MY5RKtokpFOl';
+    googleOauth2.id = '{yourGoogleApiId}';
+    googleOauth2.secret = '{yourGoogleApiSecret}';
     return googleOauth2;
 }());
 exports["default"] = googleOauth2;
