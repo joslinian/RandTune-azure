f306182998585b952fdd4da06670ea66b0f22f9b
Ian Joslin <joslinian7@gmail.com>
2018-07-11 14:49:50 -0700

    Update googleOauth2.ts

--- googleOauth2.ts
+++ googleOauth2.ts
@@ -1,6 +1,6 @@
 class googleOauth2 {
-    static id: string = '1048829641570-0o3gqdk5i3a8gfcfbk300lp4rsmdijgp.apps.googleusercontent.com';
-    static secret:string = 'aLiHEpvvXwO9MY5RKtokpFOl';
+    static id: string = '{yourGoogelApiString}';
+    static secret:string = '{yourGoogleApiSecret}';
     
     }
     export default googleOauth2;
